169885619552c2979c953b65fcd61f27632b66d3
Alex <alexanderbglover@gmail.com>
2021-09-24 16:50:16 -0400

    new routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find(params[:user_id])
+    user = User.find(params[:id])
 
     if user
       render json: user, status: :ok
